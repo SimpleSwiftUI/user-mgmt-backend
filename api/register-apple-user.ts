6e223671aee6761ce9ac15c8880b835b6fa520c3
Robert Brennan <rob22@live.ie>
2024-03-05 16:41:43 -0500

    basic register endpoint working

---
+++ api/register-apple-user.ts
@@ -0,0 +1,15 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+
+export default (req: NowRequest, res: NowResponse) => {
+    if (req.method === "POST") {
+        const { appleUserId, firstName, lastName, email } = req.body;
+  
+        console.log(`Registering user: ${appleUserId}, ${firstName}, ${lastName}, ${email}`);
+        
+        res.status(200).json({ message: `${firstName} ${lastName} registered successfully` });
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
